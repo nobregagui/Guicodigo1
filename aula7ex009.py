712c5eb0665b8fa332a041e1a4324c723f3b2bee
nobregagui <nobregagui8@gmail.com>
2020-05-28 00:04:34 -0300

    commit2

--- aula7ex009.py
+++ aula7ex009.py
@@ -1,5 +1,6 @@
 t = int(input('Digite um número:'))
 print('Dado o número {}, temos sua seguinte tabuada:{}\n {}\n {}\n {}\n {}\n {}\n {}\n {}\n {}\n {}\n'.format((t),(t*1), (t*2), (t*3), (t*4), (t*5), (t*6), (t*7), (t*8), (t*9), (t*10)))
+print('teste')
 
 
 
